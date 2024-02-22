79b2f54a12e6ae1443cccc693200dc580326b5e5
congsole <qax741@gmail.com>
2024-02-22 21:50:58 +0900

    KMDb API로 영화 목록 가져오기 / 왓챠피디아 별점 크롤링

---
+++ src/main/java/com/congsole/movie/watchaPediaDto/RateDto.java
@@ -0,0 +1,13 @@
+package com.congsole.movie.watchaPediaDto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RateDto {
+    private double rate;
+    private int rateNumber;
+}
